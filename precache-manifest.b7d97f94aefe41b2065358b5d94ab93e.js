9db4ff17274874026305d02c178b98014fd8af91
VicLo <victoria2666@gmail.com>
2020-08-25 17:50:30 -0400

    Updates

---
+++ precache-manifest.b7d97f94aefe41b2065358b5d94ab93e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b200a598d7f7da1d606e851e757d33c3",
+    "url": "/bulletin-board/index.html"
+  },
+  {
+    "revision": "c07fd872db95b611c270",
+    "url": "/bulletin-board/static/css/main.73b33d9a.chunk.css"
+  },
+  {
+    "revision": "0aa15f93e81d1b1d32ff",
+    "url": "/bulletin-board/static/js/2.91d7fb8b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bulletin-board/static/js/2.91d7fb8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c07fd872db95b611c270",
+    "url": "/bulletin-board/static/js/main.6019925c.chunk.js"
+  },
+  {
+    "revision": "855a9e905360fe51ccdc",
+    "url": "/bulletin-board/static/js/runtime-main.093f7de9.js"
+  }
+]);
